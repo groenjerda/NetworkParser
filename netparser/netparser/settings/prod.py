a9e9a59ac6f5b335424a6094d606eedf3dea61c6
MrProfessorCat <professor.cat@inbox.ru>
2024-12-10 18:16:41 +0300

    fixed workflows

--- netparser/netparser/settings/prod.py
+++ netparser/netparser/settings/prod.py
@@ -14,7 +14,7 @@ DATABASES = {
 ALLOWED_HOSTS = ['*']
 
 # https
-# CSRF_COOKIE_SECURE = True  # cookie-файлы только по протоколу HTTPS
+CSRF_COOKIE_SECURE = True  # cookie-файлы только по протоколу HTTPS
 # SESSION_COOKIE_SECURE = True  # браузеры будут передавать cookie-файлы только по протоколу HTTPS
 # SECURE_SSL_REDIRECT = True  # HTTP-запросы перенаправляются на HTTPS
 
