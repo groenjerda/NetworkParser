1c5b998a0bfd4a3e0f6287f040cc109b5ed41ddf
MrProfessorCat <professor.cat@inbox.ru>
2024-12-18 15:35:59 +0300

    test

--- netparser/netparser/settings/prod.py
+++ netparser/netparser/settings/prod.py
@@ -16,11 +16,11 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://netparser.work.gd', 'http://netparser.work.gd']
 
 # https
-# CSRF_COOKIE_SECURE = True  # cookie-файлы только по протоколу HTTPS
-# SESSION_COOKIE_SECURE = True  # браузеры будут передавать cookie-файлы только по протоколу HTTPS
+CSRF_COOKIE_SECURE = True  # cookie-файлы только по протоколу HTTPS
+SESSION_COOKIE_SECURE = True  # браузеры будут передавать cookie-файлы только по протоколу HTTPS
 
 # SECURE_SSL_REDIRECT = True  # HTTP-запросы перенаправляются на HTTPS
 
 # http
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = False
+# CSRF_COOKIE_SECURE = False
+# SESSION_COOKIE_SECURE = False
