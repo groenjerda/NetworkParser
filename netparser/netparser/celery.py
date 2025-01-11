0befa3de9c4dffc4310151e15d142c1e730cc677
MrProfessorCat <professor.cat@inbox.ru>
2025-01-11 14:18:34 +0300

    added tasks

--- netparser/netparser/celery.py
+++ netparser/netparser/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netparser.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netparser.settings.prod')
 app = Celery('netparser')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = settings.BROKER_URL
